3a853fc14f67af2b0762de3443efb6cc73a72cd8
Ask Kvarven <ask.kvarven@icloud.com>
2025-04-09 12:13:44 +0200

    Collected 700 more reviews for manual classification to be used for fine-tuning

--- scripts/extract_reviews.py
+++ scripts/extract_reviews.py
@@ -2,15 +2,19 @@ import pandas as pd
 
 # Load the datasets
 classified_df = pd.read_pickle("data/processed/bunker_test_set_tokenized.pkl")
+classified_df2 = pd.read_pickle("data/processed/bunker_test_set_tokenized2.pkl")
 all_reviews_df = pd.read_pickle("data/processed/processed_bunker_sentiment.pkl")
 
 # Exclude already classified reviews
-remaining_reviews_df = all_reviews_df[~all_reviews_df['clean_text'].isin(classified_df['clean_text'])]
+remaining_reviews_df = all_reviews_df[
+    ~all_reviews_df['clean_text'].isin(classified_df['clean_text']) &
+    ~all_reviews_df['clean_text'].isin(classified_df2['clean_text'])
+    ]
 
-# Sample 100 random reviews
-sampled_reviews_df = remaining_reviews_df.sample(n=100, random_state=42)
+# Sample x random reviews
+sampled_reviews_df = remaining_reviews_df.sample(n=700, random_state=42)
 
 # Save to pickle for further processing
-sampled_reviews_df.to_pickle("data/processed/processed_bunker_test_set2.pkl")
+sampled_reviews_df.to_pickle("data/processed/700_more_reviews_for_ft.pkl")
 
-print("New 100 reviews saved to: data/processed/processed_bunker_test_set2.pkl")
+print("New 700 reviews saved to: data/processed/700_more_reviews_for_ft.pkl")
